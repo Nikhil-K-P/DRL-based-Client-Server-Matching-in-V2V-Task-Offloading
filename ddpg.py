6a04af1576a7c6602d161442d3611c9dbc0f3df7
Nikhil-K-P <nkp897654@gmail.com>
2024-12-27 14:41:29 +0530

    Update and rename

--- ddpg.py
+++ ddpg.py
@@ -124,9 +124,3 @@ class Agent():
 
         self.target_critic.load_state_dict(critic_state_dict)
         self.target_actor.load_state_dict(actor_state_dict)
-        #self.target_critic.load_state_dict(critic_state_dict, strict=False)
-        #self.target_actor.load_state_dict(actor_state_dict, strict=False)
-
-
-
-
